6739031710b5cfed04370cbaf07148725588ba5b
cucus87 <m.sitges.arr@gmail.com>
2013-12-21 19:05:17 +0100

    added Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 OneBlogRails::Application.routes.draw do
-  resources :users
 
+  get "pages/home"
+  get "pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
