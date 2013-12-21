ac7c59f971b3a0225cf2257e3f0f0da4c14b1bf8
cucus87 <m.sitges.arr@gmail.com>
2013-12-21 18:04:11 +0100

    second try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneBlogRails::Application.config.secret_key_base = '397e372de3534ba0a9fc16f884bf453c9f255f2616093558f9892507c638e66aca45414ecb960588eadf86e425346df9075b5d9b591470da66b81c3e669c11c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OneBlogRails::Application.config.session_store :cookie_store, key: '_one_blog_rails_session'
